3356909eb039360ed94f1bdce383905a3ce2a6ab
Lucas Jarrys <lucasjarrys@hotmail.com>
2024-06-18 21:07:56 -0300

    TRABAJO FINAL YA CASI TERMINADO

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
-        mongoose.connect("");
+        mongoose.connect("mongodb+srv://admin:123@coder69930.uiaieqj.mongodb.net/proyecto3");
         console.log("MongoDB connected");
     } catch (error) {
         console.log(`Error : ${error}`);
